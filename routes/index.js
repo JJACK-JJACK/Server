c4ab9fb6f0ed172fe3fa480f2d773403f1a66022
김진희 <jin03hee16@gmail.com>
2019-07-03 20:59:12 +0900

    feat: donation_history

--- routes/index.js
+++ routes/index.js
@@ -2,5 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.use("/user", require("./user/index"));
+router.use("/donation_history", require("./donation_history/index"));
+
 
 module.exports = router;
