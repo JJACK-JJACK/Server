d5d69b7fc0223e194c998ed993939c4c89c1be65
minjung <minjony1014@gmail.com>
2019-07-10 18:05:08 +0900

    modify donate counting response message

--- routes/donate/donate.js
+++ routes/donate/donate.js
@@ -17,23 +17,26 @@ router.get('/', async (req, res) => {
     const user = jwt.verify(req.headers.token, secretOrPrivateKey);
     var resData = {};
 
-    UserHistory.find(
-        { user_id: user.userIdx }
-    ).then((history) => {
-        var programs = [];
-        history[0]["program"].forEach(function (item) {
-            programs.push(item.donateBerry);
-        });
+    UserHistory.find({
+        user_id: user.userIdx
+    }).then((history) => {
+        if (history.length == 0) {
+            res.status(200).send(util.successTrue(statusCode.OK, resMessage.READ_SUCCESS, resData));
+        } else {
+            var programs = [];
+            history[0]["program"].forEach(function (item) {
+                programs.push(item.donateBerry);
+            });
 
-        var total = 0;
-        for(var i in programs){
-            total += programs[i];
+            var total = 0;
+            for (var i in programs) {
+                total += programs[i];
+            }
+
+            resData.donateBerry = total;
+            resData.donate = programs.length;
+            res.status(200).send(util.successTrue(statusCode.OK, resMessage.READ_SUCCESS, resData));
         }
-        resData.donateBerry = total;
-        resData.donate = programs.length;
-        console.log(programs);
-
-        res.status(200).send(util.successTrue(statusCode.OK, resMessage.READ_SUCCESS, resData));
     }).catch((err) => {
         console.log(err);
         res.status(statusCode.OK).send(util.successFalse(statusCode.DB_ERROR, resMessage.READ_FAIL));
--- routes/program/program.js
+++ routes/program/program.js
@@ -7,18 +7,6 @@ const utils = require('../../module/utils/utils');
 
 const Program = require('../../models/programSchema');
 
-router.get('/', async (req, res) => {
-    //오름차순 = 1, 내림차순 = -1
-    Program.find().sort({
-            date: -1
-        })
-        .then((allPrograms) => {
-            res.status(statusCode.OK).send(utils.successTrue(statusCode.CREATED, resMessage.READ_SUCCESS, allPrograms));
-        }).catch((err) => {
-            res.status(statusCode.OK).send(utils.successFalse(statusCode.DB_ERROR, resMessage.READ_FAIL));
-        });
-});
-
 router.get('/detail/:programId', async (req, res) => {
 
     Program.find({
@@ -91,8 +79,26 @@ router.get('/:categoryId/:filterId', async (req, res) => {
 });
 
 router.post('/', (req, res) => {
-    
+
     Program.create(req.body)
+        .then((result) => {
+            res.status(statusCode.OK).send(utils.successTrue(statusCode.CREATED, resMessage.SAVE_SUCCESS, result));
+        }).catch((err) => {
+            console.log(err);
+            res.status(statusCode.OK).send(utils.successFalse(statusCode.DB_ERROR, resMessage.SAVE_FAIL));
+        });
+});
+
+router.get('/', (req, res) => {
+    Program.aggregate([
+            {
+                $group: {
+                    _id: "기부 총 금액",
+                    totalDonate: {
+                        $sum: "$totalBerry"
+                    }, 
+                }
+            }])
         .then((result) => {
             res.status(statusCode.OK).send(utils.successTrue(statusCode.CREATED, resMessage.SAVE_SUCCESS, result));
         }).catch((err) => {
