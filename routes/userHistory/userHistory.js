222aca4be34f654492540074fc1834429f9399eb
minjung <minjony1014@gmail.com>
2019-07-10 16:42:32 +0900

    stamps reset if their value is 10

--- routes/userHistory/userHistory.js
+++ routes/userHistory/userHistory.js
@@ -5,9 +5,12 @@ const resMessage = require('../../module/utils/responseMessage');
 const statusCode = require('../../module/utils/statusCode');
 const util = require('../../module/utils/utils');
 
+const pool = require('../../module/pool');
+
 const Program = require('../../models/programSchema');
 const UserHistory = require('../../models/userHistorySchema');
 const Stamp = require('../../models/stampSchema');
+const BerryHistory = require('../../models/berryHistorySchema');
 
 const jwt = require('jsonwebtoken');
 const secretKey = "jwtSecretKey!";
@@ -19,6 +22,9 @@ moment.tz.setDefault("Asia/Seoul");
 router.post('/:programId', (req, res) => {
 
     const user = jwt.verify(req.headers.token, secretKey);
+
+    var rewardBerry = 0;
+    var stamps = 0;
 
     if (user === null || !req.body.donateBerry) {
         res.status(statusCode.OK).send(util.successFalse(statusCode.BAD_REQUEST, resMessage.NULL_VALUE));
@@ -45,7 +51,6 @@ router.post('/:programId', (req, res) => {
                         runValidators: true
                     })
                     .then((result) => {
-
                         Stamp.findOneAndUpdate({
                                 user_id: user.userIdx
                             }, {
@@ -58,12 +63,71 @@ router.post('/:programId', (req, res) => {
                                 upsert: true,
                                 runValidators: true
                             })
-                            .then((result) => {
-                                if (result[0].cntStamp == 10) {
+                            .then(async (result) => {
+                                stamps = result.cntStamp;
+                                if(result.cntStamp >= 10){
+                                    var random = Math.floor(Math.random() * 10) + 1;
+                                    var reward = Math.floor((result.totalBerry) * (random / 100));
+    
+                                    BerryHistory.findOneAndUpdate({
+                                        user_id: user.userIdx
+                                    }, {
+                                        $push: {
+                                            charge: {
+                                                "title": "베리 수확",
+                                                "chargeBerry": reward,
+                                                "date": moment().format('YYYY-MM-DD HH:mm:ss'),
+                                            }
+                                        }
+                                    }, {
+                                        upsert: true,
+                                        new: true,
+                                        runValidators: true
+                                    })
+                                    .then(async (result) => {
+                                        rewardBerry = reward;
 
-                                } else {
-                                    res.status(statusCode.OK).send(util.successTrue(statusCode.CREATED, resMessage.SAVE_SUCCESS, result));
+                                        Stamp.findOneAndUpdate({
+                                            user_id: user.userIdx
+                                        }, {
+                                        totalBerry : 0, cntStamp : 0
+                                        }, {
+                                            upsert: true,
+                                            new: true,
+                                            runValidators: true
+                                        })
+                                        .then(async (result) => {
+                                            rewardBerry = reward;
+    
+                                            
+                                        }).catch((err) => {
+                                            res.status(statusCode.OK).send(util.successFalse(statusCode.DB_ERROR, resMessage.SAVE_FAIL));
+                                        });
+
+                                    }).catch((err) => {
+                                        res.status(statusCode.OK).send(util.successFalse(statusCode.DB_ERROR, resMessage.SAVE_FAIL));
+                                    });
                                 }
+
+                                const UpdateBerryQuery = 'UPDATE User SET myBerry = myBerry - ? WHERE userIdx = ?';
+                                await pool.queryParam_Parse(UpdateBerryQuery, [req.body.donateBerry, user.userIdx]);
+
+                                Program.findOneAndUpdate({
+                                    _id: req.params.programId
+                                }, {
+                                    $inc: {
+                                        totalBerry: req.body.donateBerry,
+                                    }
+                                }, {
+                                    new: true,
+                                    upsert: true,
+                                    runValidators: true
+                                }) .then(async (result) => {
+                                    res.status(statusCode.OK).send(util.successTrue(statusCode.CREATED, resMessage.SAVE_SUCCESS, {"totalBerry" : result.totalBerry, "stamps": stamps, "rewordsBerry" : rewardBerry }));
+                                    console.log(err);
+                                    res.status(statusCode.OK).send(util.successFalse(statusCode.DB_ERROR, resMessage.READ_FAIL));
+                                });
+
                             }).catch((err) => {
                                 console.log(err);
                                 res.status(statusCode.OK).send(util.successFalse(statusCode.DB_ERROR, resMessage.READ_FAIL));
